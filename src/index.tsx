5c4cda674a422da577f1854f9b2a8d4a6b8c8525
mostlyfocusedmike <mostlyfocusedmike@gmail.com>
2019-09-15 18:33:09 -0400

    alter setup to get it working

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,12 @@ import * as React from "react";
 import * as ReactDOM from "react-dom";
 
 import { Hello } from "./components/Hello";
+import { Goodbye } from "./components/Goodbye";
 
 ReactDOM.render(
-    <Hello compiler="TypeScript" framework="React" />,
+    <div>
+        <Hello compiler="TypeScript" framework="React" />
+        <Goodbye compiler="TypeScript" framework="React" />
+    </div>,
     document.getElementById("example")
 );
